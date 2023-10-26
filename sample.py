4337a6721e5ea3360de3b7463c87b3c07806bf34
Javier Gómez <javier.gomez@mitigasolutions.com>
2023-10-26 13:00:28 +0200

    cambiado churros a porras

--- sample.py
+++ sample.py
@@ -8,22 +8,22 @@ import mlogs
 #    )
 logger = mlogs.get_logger("experimental")
 
-class churro():
+class porra():
     def __init__(self, name, price):
         self.name = name
         self.price = price
-        logger.info("Churro creado: {} (${})".format(self.name, self.price))
+        logger.info("porra creada: {} (${})".format(self.name, self.price))
 
     def make(self, quantity=1):
-        logger.warning("Haz {} churro(s) {}".format(quantity, self.name))
+        logger.warning("Haz {} porra(s) {}".format(quantity, self.name))
 
     def eat(self, quantity=1):
-        logger.critical("come {} churro(s) {} ".format(quantity, self.name))
+        logger.critical("come {} porra(s) {} ".format(quantity, self.name))
 
-churro_01 = churro("Sencillo", 15)
-churro_01.make(5)
-churro_01.eat(4)
+porra_01 = porra("Sencillo", 15)
+porra_01.make(5)
+porra_01.eat(4)
 
-churro_02 = churro("Relleno", 16)
-churro_02.make(2)
-churro_02.eat(2)
+porra_02 = porra("Relleno", 16)
+porra_02.make(2)
+porra_02.eat(2)
